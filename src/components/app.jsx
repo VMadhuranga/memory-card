ea6367c482cc40edc3facb9fa5f3a4d64f43a34e
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 08:02:54 +0530

    Update App component
    Create pokemonData state variable and pass empty array as
    initial state
    
    Initialize useEffect to update pokemonData state
    using return value of fetchPokemonData function

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,8 +1,25 @@
-import { pokemonData } from "../data/pokemon-data";
+import { fetchPokemonData } from "../data/pokemon-data";
 import { CardContainer } from "./card-container/card-container";
 import { ScoreBoard } from "./score-board/score-board";
+import { useEffect, useState } from "react";
 
 export function App() {
+  const [pokemonData, updatePokemonData] = useState([]);
+
+  useEffect(() => {
+    let ignore = false;
+
+    fetchPokemonData().then((res) => {
+      if (!ignore) {
+        updatePokemonData([...res]);
+      }
+    });
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
   return (
     <>
       <header>
