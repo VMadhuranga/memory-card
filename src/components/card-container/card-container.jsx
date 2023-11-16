9baf55cb086025adb4047450e57878456707c242
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 12:36:59 +0530

    Create shufflePokemonData function and pass it to Card component
    This function shuffles Pokemon data so each time pokemon card click
    it will render shuffled card

--- src/components/card-container/card-container.jsx
+++ src/components/card-container/card-container.jsx
@@ -1,7 +1,24 @@
 import { Card } from "../card/card";
 
-export function CardContainer({ pokemonData, handleScore }) {
+export function CardContainer({ pokemonData, handleScore, updatePokemonData }) {
   const cards = [];
+
+  function shufflePokemonCards() {
+    const shuffle = [...pokemonData].reduce(
+      (acc, cur, index) => {
+        if (index % 2 === 0) {
+          acc.even.push(cur);
+        } else if (index % 2 !== 0) {
+          acc.odd.push(cur);
+        }
+
+        return acc;
+      },
+      { odd: [], even: [] },
+    );
+
+    updatePokemonData([...shuffle.odd, ...shuffle.even]);
+  }
 
   pokemonData.forEach((pokemon) => {
     cards.push(
@@ -10,7 +27,7 @@ export function CardContainer({ pokemonData, handleScore }) {
         src={pokemon.imageURL}
         alt={pokemon.name}
         name={pokemon.name}
-        handleClick={handleScore}
+        handleClick={{ handleScore, shufflePokemonCards }}
       ></Card>,
     );
   });
