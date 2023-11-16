27b6416f018e5c686ecea83d9c80c527c080bf25
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 19:20:28 +0530

    Add styling to ScoreBoard component

--- src/components/score-board/score-board.jsx
+++ src/components/score-board/score-board.jsx
@@ -1,6 +1,8 @@
+import "./score-board.css";
+
 export function ScoreBoard({ currentScore, bestScore }) {
   return (
-    <div>
+    <div className="score-board">
       <p>Current Score: {currentScore}</p>
       <p>Best Score: {bestScore}</p>
     </div>
