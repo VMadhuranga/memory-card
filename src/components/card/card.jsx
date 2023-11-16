48ed9e5bf4857b97894845030f9b488fff644d36
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 09:58:00 +0530

    Add click event listener
    Create handleClick prop pass down a event handler to this event
    listener

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,14 @@
-export function Card({ src, alt, name }) {
+export function Card({ src, alt, name, handleClick }) {
   return (
     <div>
-      <img src={src} alt={alt} width={"200px"} height={"200px"} />
+      <img
+        src={src}
+        alt={alt}
+        width={"200px"}
+        height={"200px"}
+        data-name={name}
+        onClick={handleClick}
+      />
       <p>{name}</p>
     </div>
   );
