0a9af6ac53ae88b181db7997f2c4a19751adb8dc
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 19:41:06 +0530

    Add width and height to img element improve bit of performance

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,11 +6,14 @@ export function Card({ src, alt, name, handleClick }) {
       <img
         src={src}
         alt={alt}
+        width={"200px"}
+        height={"200px"}
         data-name={name}
         onClick={(e) => {
           handleClick.handleScore(e);
           handleClick.shufflePokemonCards();
         }}
+        loading="lazy"
       />
       <p>{name[0].toUpperCase() + name.slice(1)}</p>
     </div>
