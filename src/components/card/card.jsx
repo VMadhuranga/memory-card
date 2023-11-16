8f49fdcdc9046fe925d5768f01dc8c5047dade8c
VMadhuranga <131940995+VMadhuranga@users.noreply.github.com>
2023-11-16 08:00:46 +0530

    Give img element a width and height attribute

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,7 +1,7 @@
 export function Card({ src, alt, name }) {
   return (
     <div>
-      <img src={src} alt={alt} />
+      <img src={src} alt={alt} width={"200px"} height={"200px"} />
       <p>{name}</p>
     </div>
   );
